81a865110761dc6a30dfbdf501c21c9aa4aa4e1a
sherinmn <58552804+sherinmn@users.noreply.github.com>
2021-12-31 15:17:19 +0300

    Add files via upload

---
+++ employee-management-design/src/main/java/com/greatlearning/employee/entity/User.java
@@ -0,0 +1,76 @@
+package com.greatlearning.employee.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.greatlearning.employee.entity.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private int id;
+	private String username;
+	private String password;
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	List<Role> roles = new ArrayList<Role>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getusername() {
+		return username;
+	}
+
+	public void setusername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public User(String username, String password, List<Role> roles) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public User() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", roles=" + roles + "]";
+	}
+
+}
